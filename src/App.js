4aa7d15a7151e6a72df41bf23e75d7a15e7e4be2
m.abdul@oro24.com <m.abdul@oro24.com>
2024-11-28 15:44:35 +0400

    Chnages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> I am Muqeet. deploy from actions.
+          Edit <code>src/App.js</code> I am Muqeet. deploy from actions. dvsds
         </p>
         <a
           className="App-link"
