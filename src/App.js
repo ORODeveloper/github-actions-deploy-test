21583cd96f9bd58d6d788bc483fefabb7632e6d9
m.abdul@oro24.com <m.abdul@oro24.com>
2024-11-28 15:57:37 +0400

    staging

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> I am Muqeet. deploy from actions. Hello Stagging --------------- _____++++++++++++ +++_ dfvds
+          Edit <code>src/App.js</code> I am Muqeet. deploy from actions. Hello Stagging 
         </p>
         <a
           className="App-link"
